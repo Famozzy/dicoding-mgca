6688af382304a4cd57c073755db6f6dfd4dd352a
Faidil <faidilf@proton.me>
2024-12-31 09:17:32 +0800

    chore: terraform format

--- build-reliable-architecture/load-balancer.tf
+++ build-reliable-architecture/load-balancer.tf
@@ -65,8 +65,8 @@ resource "google_compute_global_forwarding_rule" "https_forwarding_rule" {
 
 # --- HTTP Load Balancer ---
 resource "google_compute_target_http_proxy" "http_proxy" {
-  name             = "http-proxy"
-  url_map          = google_compute_url_map.lb_url_map.self_link
+  name    = "http-proxy"
+  url_map = google_compute_url_map.lb_url_map.self_link
 }
 
 resource "google_compute_global_forwarding_rule" "http_forwarding_rule" {
